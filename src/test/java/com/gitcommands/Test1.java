24100e6b8cd64002aaf5b4b9f07f9a4511fef877
Prantik Pattanayak <pattanayakprantik@gmail.com>
2021-03-20 17:18:03 +0530

    GitCommands

---
+++ src/test/java/com/gitcommands/Test1.java
@@ -0,0 +1,10 @@
+package com.gitcommands;
+
+public class Test1 
+{
+     public static void main(String[] args) 
+     {
+		System.out.println("Hi");
+		System.out.println("Git");
+	 }
+}
---
+++ src/test/java/com/gitcommands/Test2.java
@@ -0,0 +1,10 @@
+package com.gitcommands;
+
+public class Test2 
+{
+	 public static void main(String[] args) 
+     {
+		System.out.println("Hello");
+		System.out.println("Github");
+	 }
+}
