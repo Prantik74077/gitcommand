43dcefd3de94db7f9ff9bc86143eae76e8bf4f4e
Prantik Pattanayak <pattanayakprantik@gmail.com>
2021-03-20 17:29:50 +0530

    Adding 3rd class

---
+++ src/test/java/com/gitcommands/Test3.java
@@ -0,0 +1,11 @@
+package com.gitcommands;
+
+public class Test3 
+{
+	public static void main(String[] args) 
+    {
+		System.out.println("Hello");
+		System.out.println("Github");
+		System.out.println("3rd class");
+	}
+}
